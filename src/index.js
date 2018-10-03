8963575b000ba115cf896a4bf2ec9a2b1bffc3a7
Nikita Valkevich <n.valkevich@icloud.com>
2018-10-03 21:22:27 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  // your implementation\
+  var arrFac = [];
+
+  for (var i = 2; i <= base; i++){
+  	if(base % i != 0) continue;
+  	var k = 0;
+  	while (base % i == 0){
+  		base = base / i;
+  		k++;
+  	}
+  	arrFac.push({value: i, count: k});
+  }
+
+  countArr = [];
+
+  arrFac.forEach(factor => {
+  	var count = 0;
+  	var temp = number;
+  	while (temp/factor.value != 0){
+  		count = count + Math.floor(temp/factor.value);
+  		temp = Math.floor(temp/factor.value);
+  	}
+  	countArr.push(Math.floor(count/factor.count));
+  }) ;
+  return Math.min.apply(null, countArr);
 }
